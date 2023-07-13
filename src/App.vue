8ab95235ae12a2f440cd4bdfc62be1f6255e008a
Yh <694105460@qq.com>
2023-07-13 17:36:10 +0800

    swipe 7/13

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="app">
     <div class="header">
-      
+      <van-icon name="bars" size="30" color="gray" />
+      <img src="@/assets/lgoo.png" alt="">
+      <van-badge :content="0" color="#1989fa">
+        <van-icon name="shopping-cart" size="30" color="rgb(236, 156, 7);" />
+      </van-badge>
     </div>
-    <router-view />
-    <div class="bottomn">
+    <div class="main">
+      <router-view />
+    </div>
+    <div class="bottom">
 
     </div>
   </div>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,34 @@
 <template>
-   <div>
-    home
+   <div class="home">
+      <div class="swipe-box">
+         <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+         <van-swipe-item>1</van-swipe-item>
+         <van-swipe-item>2</van-swipe-item>
+         <van-swipe-item>3</van-swipe-item>
+         <van-swipe-item>4</van-swipe-item>
+      </van-swipe>
+      </div>
    </div>
 </template>
 
 
 <script setup>
-import { ref, reactive, toRefs, onMounted} from 'vue'
+import { ref, reactive, toRefs, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 </script>
 
 
 <style lang='less' scoped>
-
+.swipe-box{
+   padding: 0.5rem;
+}
+.my-swipe .van-swipe-item {
+   color: #fff;
+   font-size: 20px;
+   height: 200px;
+   line-height: 150px;
+   text-align: center;
+   background-color: #39a9ed;
+}
 </style>
